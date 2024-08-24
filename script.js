6ce42762b9b8c0f7e92842129d42559c69c06516
Antonio Quistelli <antonio.quistelli@gmail.com>
2024-08-24 14:23:24 +0200

    add reset button and fixed the css

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-
-
 function createCounterUI() {
     const container = document.getElementById('counter-app');
 
+   
     const title = document.createElement('h1');
     title.innerText = 'Counter';
     container.appendChild(title);
 
+    
     const counterDisplay = document.createElement('span');
     counterDisplay.id = 'counter';
-    counterDisplay.innerText = '0'; 
+    counterDisplay.innerText = '0';
     container.appendChild(counterDisplay);
 
+    
+    const buttonContainer = document.createElement('div');
+    buttonContainer.className = 'button-container';
+
+   
     const decrementButton = document.createElement('button');
     decrementButton.innerText = '-';
     decrementButton.onclick = decrementCounter;
-    container.appendChild(decrementButton);
+    buttonContainer.appendChild(decrementButton);
 
+    
     const incrementButton = document.createElement('button');
     incrementButton.innerText = '+';
     incrementButton.onclick = incrementCounter;
-    container.appendChild(incrementButton);
+    buttonContainer.appendChild(incrementButton);
+
+  
+    const resetButton = document.createElement('button');
+    resetButton.innerText = 'Reset';
+    resetButton.className = 'reset';
+    resetButton.onclick = resetCounter;
+    buttonContainer.appendChild(resetButton);
+
+  
+    container.appendChild(buttonContainer);
 }
-
-
 let counterValue = 0;
-
 
 function updateCounterDisplay() {
     const counterDisplay = document.getElementById('counter');
     counterDisplay.innerText = counterValue;
 }
-
-
 function incrementCounter() {
     counterValue++;
     updateCounterDisplay();
 }
-
 
 function decrementCounter() {
     counterValue--;
@@ -45,4 +55,10 @@ function decrementCounter() {
 }
 
 
-document.addEventListener('DOMContentLoaded', createCounterUI);
+function resetCounter() {
+    counterValue = 0;
+    updateCounterDisplay();
+}
+
+
+createCounterUI();
